d8f97276f026526ae2a3de1e43f1b86e4cbdc24f
mohamedBoutaieb <mohamed@letsfluence.com>
2024-04-15 15:50:11 +0200

    config : routing config and home component

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,23 @@
-import { useState } from "react";
+import { Suspense, useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
+import { BrowserRouter, Route, Router, Routes } from "react-router-dom";
+import Home from "./components/Home";
 
 function App() {
   const [count, setCount] = useState(0);
 
   return (
     <>
-      <div className="text-red-600  text-6xl">
-           Green is the new red
-      </div>
+      <BrowserRouter>
+        <Suspense fallback={<div>Loading...</div>}>
+          <Routes>
+            <Route index element={<Home />} />
+          </Routes>
+        </Suspense>
+      </BrowserRouter>
+    
     </>
   );
 }
---
+++ src/components/Home.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+
+
+
+
+const Home = () => {
+    return (
+      <div className="w-screen h-screen border-2" 
+      >
+        weclome to the home page
+      </div>
+    );
+  };
+  
+  export default Home;
+  
